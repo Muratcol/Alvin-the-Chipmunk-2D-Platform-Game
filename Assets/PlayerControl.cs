a1e4dad2a734918c84e661b1afa89c98d8304a19
Muratcol <muratcolyaran@yahoo.com.tr>
2020-07-31 21:18:52 +0300

    Basic movements

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, -1);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, -1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, -1);
+        }
+
+
+    }
+}
