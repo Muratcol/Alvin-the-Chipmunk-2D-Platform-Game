2cba59815689598814e1d278c4d00c170081372a
Muratcol <muratcolyaran@yahoo.com.tr>
2020-08-02 20:00:09 +0300

    Movement scripts update

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,19 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.ComTypes;
+﻿
+using System.Numerics;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.SocialPlatforms;
-
-
-
-
+using Vector3 = UnityEngine.Vector3;
+using Vector2 = UnityEngine.Vector3;
 
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rb;
     private Animator anim;
+    private Collider2D coll;
+
     private enum State {idle, running, jumping}
     private State state = State.idle;
     private void Start()
@@ -22,49 +19,47 @@ public class PlayerControl : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-
-    // Movement Function
-    Vector2 Movement(float x, float y)
-    {
-        return new Vector2(x, y);
-    }
+    private float jumpHeight = 5f;
+    private static bool isJumping = false; 
 
 
     // Update is called once per frame
     void Update()
     {
-        int moveSpeed = 5;
-        int jumpVelocity = 30;
-        
+        float moveSpeed = 5f;
         float hDirection = Input.GetAxis("Horizontal");
         float vDirection = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = Movement(-moveSpeed, jumpVelocity);
-            state = State.jumping;
-        }
+        Jump();
+
+        Vector3 movement = new Vector3(hDirection, 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
 
         if (hDirection < 0)
         {
             transform.localScale = new Vector2(-1, 1);
-            rb.velocity = Movement(-moveSpeed, rb.velocity.y);
             state = State.running;
-
         }
         else if(hDirection > 0)
         {
             transform.localScale = new Vector2(1, 1);
-            rb.velocity = Movement(moveSpeed, rb.velocity.y);
             state = State.running;
         }
         else
         {
-            rb.velocity = Movement(0, rb.velocity.y);
             state = State.idle;
         }
         anim.SetInteger("state", (int)state);
 
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+        
     }
 
 
