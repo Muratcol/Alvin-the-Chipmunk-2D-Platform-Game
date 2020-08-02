ab47cde64136b51e9277932b2c770904d29d3dc6
Muratcol <muratcolyaran@yahoo.com.tr>
2020-08-02 11:27:56 +0300

    Sprite flippings

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,26 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+
+
+
 
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody2D rb;
 
+
+    // Movement Function
+    Vector2 Movement(float x, float y)
+    {
+        return new Vector2(x, y);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
+        int moveSpeed = 5;
+        int jumpVelocity = 30;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Movement(-moveSpeed, jumpVelocity);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector2(-5, -1);
+            transform.localScale = new Vector2(-1, 1);
+            rb.velocity = Movement(-moveSpeed, rb.velocity.y);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector2(5, -1);
+            transform.localScale = new Vector2(1, 1);
+            rb.velocity = Movement(moveSpeed, rb.velocity.y);
         }
         else
         {
-            rb.velocity = new Vector2(0, -1);
+            rb.velocity = Movement(0, rb.velocity.y);
         }
 
 
