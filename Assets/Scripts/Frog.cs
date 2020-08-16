924501a386027c5ee64f9276d820e0871c8b2a99
Muratcol <muratcolyaran@yahoo.com.tr>
2020-08-16 18:12:06 +0300

    Frog animations and roam route developing

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Frog : MonoBehaviour
 {
+    [SerializeField] private float enemyMoveSpeed = 5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class Frog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+
+    void movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(0, 1f, 0f);
+        transform.position += movement * Time.deltaTime * enemyMoveSpeed;
     }
 }
