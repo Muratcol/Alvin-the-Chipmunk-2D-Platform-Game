6368ea3a2e5d91109a66ad66f90ce0d4fadaca62
Muratcol <muratcolyaran@yahoo.com.tr>
2020-08-20 15:00:58 +0300

    New enemy added to game.

--- Assets/Scripts/Opossum.cs
+++ Assets/Scripts/Opossum.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Opossum : MonoBehaviour
 {
+    private Collider2D coll;
+
+    [SerializeField] private float jumpLength;
+    [SerializeField] private float leftWaypoint;
+    [SerializeField] private float rightWaypoint;
+    public Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private LayerMask ground;
+
+    private bool facingLeft = true;
+
+    private enum State { idle, jumping, falling }
+    private State state = State.idle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (transform.position.x <= leftWaypoint)
+        {
+            transform.localScale = new Vector2(-1, 1);
+            facingLeft = false;
+            movement();
+        }
+        else if (transform.position.x >= rightWaypoint)
+        {
+            transform.localScale = new Vector2(1, 1);
+            facingLeft = true;
+            movement();
+        }
+
+    }
+
+    void movement()
+    {
+        if (coll.IsTouchingLayers(ground) && facingLeft)
+        {
+            rb.velocity = new Vector2(-jumpLength, 0);
+        }
+        else if (coll.IsTouchingLayers(ground) && !facingLeft)
+        {
+            rb.velocity = new Vector2(jumpLength, 0);
+        }
+
     }
 }
