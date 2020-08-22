a6dfbfc3efb8f3be9c77931add47aabc79e530d4
Muratcol <muratcolyaran@yahoo.com.tr>
2020-08-22 13:41:45 +0300

    Code cleanup update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    protected Animator anim;
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+
+    public void JumpedOn()
     {
-        
+        anim.SetTrigger("Death");
+    }
+    private void Death()
+    {
+        Destroy(this.gameObject);
     }
 }
